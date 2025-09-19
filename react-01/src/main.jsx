1b0b9d2c7aed45521548cc4b89e72c4bf278dafe
Sumit <sumitsahu7504@gmail.com>
2025-09-19 14:47:38 +0530

    Update main.jsx to include Counter and QCounter components

--- react-01/src/main.jsx
+++ react-01/src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Counter from "./Counter.jsx";
+import QCounter from "./Q1.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Counter />
+    <QCounter />
   </React.StrictMode>
 );
