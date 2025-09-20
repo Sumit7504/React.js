db443d7c08b02b70b8cda0e430f32b1f62e6a13e
Sumit <sumitsahu7504@gmail.com>
2025-09-21 01:11:49 +0530

    Add Project Based on background color changer functionality using useState and Tailwind CSS

--- react-02/src/App.jsx
+++ react-02/src/App.jsx
@@ -1,7 +1,62 @@
+// This Project are basically for changing the background color of the screen using reactjs and tailwindcss.
+// Here we are using useState hook for changing the color of the screen.
+
 import React from "react";
+import { useState } from "react";
 
 function App() {
-  return <h1 className="text-blue-800 bg-amber-400">Hello world!</h1>;
+  const [color, setColor] = useState("white");
+
+  return (
+    <div
+      className=" w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-amber-50 px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className=" outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ background: "red" }}
+          >
+            Red
+          </button>
+
+          <button
+            onClick={() => setColor("Green")}
+            className=" outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ background: "Green" }}
+          >
+            Green
+          </button>
+
+          <button
+            onClick={() => setColor("Blue")}
+            className=" outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ background: "Blue" }}
+          >
+            Blue
+          </button>
+
+          <button
+            onClick={() => setColor("Black")}
+            className=" outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ background: "Black" }}
+          >
+            Black
+          </button>
+
+          <button
+            onClick={() => setColor("Yellow")}
+            className=" outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ background: "yellow" }}
+          >
+            Yellow
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
